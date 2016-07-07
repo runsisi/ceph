41d771bfd121f7d256fa4e4650fdb9e37f4fbb9d
runsisi <runsisi@hust.edu.cn>
2016-07-07 20:23:00 +0800

    update notes
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- src/librados/librados.cc
+++ src/librados/librados.cc
@@ -1476,10 +1476,12 @@ int librados::IoCtx::aio_operate(const std::string& oid, AioCompletion *c,
 				 snap_t snap_seq, std::vector<snap_t>& snaps)
 {
   object_t obj(oid);
+
   vector<snapid_t> snv;
   snv.resize(snaps.size());
   for (size_t i = 0; i < snaps.size(); ++i)
     snv[i] = snaps[i];
+
   SnapContext snapc(snap_seq, snv);
   return io_ctx_impl->aio_operate(obj, &o->impl->o, c->pc,
 				  snapc, 0);
