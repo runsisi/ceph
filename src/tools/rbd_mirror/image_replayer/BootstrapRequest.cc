1c28dc111e1dbb4ac8aa95174e75df036a7d26f8
runsisi <runsisi@zte.com.cn>
2016-07-16 12:16:35 +0800

    update notes
    Signed-off-by: runsisi <runsisi@zte.com.cn>

--- src/tools/rbd_mirror/image_replayer/BootstrapRequest.cc
+++ src/tools/rbd_mirror/image_replayer/BootstrapRequest.cc
@@ -545,6 +545,7 @@ void BootstrapRequest<I>::get_remote_tags() {
   Context *ctx = create_context_callback<
     BootstrapRequest<I>, &BootstrapRequest<I>::handle_get_remote_tags>(this);
 
+  // the remote tag class was got by BootstrapRequest<I>::get_remote_tag_class
   m_journaler->get_tags(m_remote_tag_class, &m_remote_tags, ctx);
 }
 
@@ -604,6 +605,7 @@ void BootstrapRequest<I>::handle_get_remote_tags(int r) {
       return;
     }
 
+    // local_image_ctx->journal has been opened by BootstrapRequest<I>::open_local_image
     librbd::journal::TagData tag_data =
       local_image_ctx->journal->get_tag_data();
 
