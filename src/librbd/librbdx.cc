cbcae5565d30d75187703aed1b0ad5ff0d29ac5b
runsisi <runsisi@hust.edu.cn>
2019-11-13 11:59:21 +0800

    librbdx: add simplified API for image batch queries
    Change-Id: Id333e580d112721322434a8096449b0c9e836a90
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

---
+++ src/librbd/librbdx.cc
@@ -0,0 +1,45 @@
+/*
+ * librbdx.cc
+ *
+ *  Created on: Jul 31, 2019
+ *      Author: runsisi
+ */
+
+#include "include/rbd/librbdx.hpp"
+#include "librbd/api/xImage.h"
+
+namespace librbdx {
+
+int xRBD::get_info(librados::IoCtx& ioctx,
+    const std::string& image_id, image_info_t* info) {
+  int r = 0;
+  r = librbd::api::xImage<>::get_info(ioctx, image_id, info);
+  return r;
+}
+
+int xRBD::list(librados::IoCtx& ioctx,
+    std::map<std::string, std::string>* images) {
+  int r = 0;
+  images->clear();
+  r = librbd::api::xImage<>::list(ioctx, images);
+  return r;
+}
+
+int xRBD::list_info(librados::IoCtx& ioctx,
+    std::map<std::string, std::pair<image_info_t, int>>* infos) {
+  int r = 0;
+  infos->clear();
+  r = librbd::api::xImage<>::list_info(ioctx, infos);
+  return r;
+}
+
+int xRBD::list_info(librados::IoCtx& ioctx,
+    const std::map<std::string, std::string>& images,
+    std::map<std::string, std::pair<image_info_t, int>>* infos) {
+  int r = 0;
+  infos->clear();
+  r = librbd::api::xImage<>::list_info(ioctx, images, infos);
+  return r;
+}
+
+}
